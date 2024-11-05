e82b41209053196a810596aa54b26f52059f68eb
Vishal <vishalpal1501@gmail.com>
2024-11-05 13:00:55 +0530

    designing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { Editor } from 'draft-js';
+// import { Editor } from 'draft-js';
 import './App.css';
 import Button from './components/Button';
 import Title from './components/Title';
-import 'draft-js/dist/Draft.css';
+// import 'draft-js/dist/Draft.css';
 
 function App() {
   return (
-    <main>
+    <main
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        margin: 'auto',
+        padding: '10px',
+      }}
+    >
       <Title />
-      <Button />
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'end' }}>
+        <Button />
+      </div>
       <div
         style={{
+          marginTop: '10px',
           border: '1px solid #ddd',
-          padding: '10px',
-          minHeight: '200px',
+          minHeight: '300px',
+          width: '100%',
         }}
       >
-        <Editor />
+        {/* <Editor /> */}
       </div>
     </main>
   );
