d10715bc0461551cd3cbe93f92d614cf70735e76
Vishal <vishalpal1501@gmail.com>
2024-11-05 12:47:50 +0530

    create components and import Editor from draft-js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Editor } from 'draft-js';
 import './App.css';
+import Button from './components/Button';
+import Title from './components/Title';
+import 'draft-js/dist/Draft.css';
 
 function App() {
   return (
-    <>
-      <h1>Zicuro Technologies Editor</h1>
-    </>
+    <main>
+      <Title />
+      <Button />
+      <div
+        style={{
+          border: '1px solid #ddd',
+          padding: '10px',
+          minHeight: '200px',
+        }}
+      >
+        <Editor />
+      </div>
+    </main>
   );
 }
 
